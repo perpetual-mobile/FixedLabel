04cc4fae0bbc7a0358d0391db04f59d0e48ab012
Falko Schindler <mail@falkoschindler.de>
2015-05-11 11:28:30 +0200

    initial commit

---
+++ Demo/Demo.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace Demo
+{
+    public class App : Application
+    {
+        public App()
+        {
+            var stack = new StackLayout { VerticalOptions = LayoutOptions.Center };
+            for (var i = 0; i < 40; i++)
+                stack.Children.Add(new Label { Text = "Label " + i });
+
+            MainPage = new ContentPage {
+                Padding = new Thickness(0, Device.OS == TargetPlatform.iOS ? 20 : 0, 0, 0),
+                Content = new ScrollView{ Content = stack },
+            };
+        }
+    }
+}
+
